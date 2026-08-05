b0dba60f870d94cf6768437dae803acfdb069357
Anastasiia <a.liashchevska@gmail.com>
2026-08-05 21:19:29 +0300

    test: add client fixture and reconfigure engine fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,46 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from app.database import Base
+from sqlalchemy.pool import StaticPool
+from app.database import Base, get_session
+from app.main import app
+from fastapi.testclient import TestClient
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def engine():
-    engine = create_engine('sqlite+pysqlite:///:memory:')
+    engine = create_engine(
+        "sqlite+pysqlite:///:memory:",
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,
+    )
     Base.metadata.create_all(engine)
     yield engine
     Base.metadata.drop_all(engine)
     engine.dispose()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture
 def session(engine):
     connection = engine.connect()
     transaction = connection.begin()
-    
+
     session = Session(bind=connection, expire_on_commit=False)
     yield session
-    
+
     session.close()
     transaction.rollback()
     connection.close()
+
+
+@pytest.fixture
+def client(session):
+    def override_get_session():
+        yield session
+
+    app.dependency_overrides[get_session] = override_get_session
+
+    with TestClient(app) as client:
+        yield client
+
+    app.dependency_overrides.clear()
