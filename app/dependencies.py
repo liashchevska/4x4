2328ff31f016073856a2f94cb4db241b2d7cd1c0
Anastasiia <a.liashchevska@gmail.com>
2026-08-05 18:56:36 +0300

    refactor: rename session_dependency to SessionDependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -3,4 +3,4 @@ from sqlalchemy.orm import Session
 from fastapi import Depends
 from app.database import get_session
 
-session_dependency = Annotated[Session, Depends(get_session)]
+SessionDependency = Annotated[Session, Depends(get_session)]
